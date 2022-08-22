119a5cf620feffb2b7a980051a05a377acb77c9d
Pouria Delfanazari <pdelfan@gmail.com>
2022-08-21 22:27:11 -0700

    Installed packages, added storybook, tokens

---
+++ .storybook/customViewports.js
@@ -0,0 +1,72 @@
+export const customViewports = {
+    iphoneSE: {
+      name: "iPhone SE",
+      styles: {
+        width: "320px",
+        height: "568px",
+      },
+    },
+    iphoneSE2: {
+      name: "iPhone SE (2nd/3rd Gen)",
+      styles: {
+        width: "375px",
+        height: "667px",
+      },
+    },
+    iphoneX: {
+      name: "iPhone X",
+      styles: {
+        width: "375px",
+        height: "812px",
+      },
+    },
+    iphone12Pro: {
+      name: "iPhone 12 Pro",
+      styles: {
+        width: "390px",
+        height: "844px",
+      },
+    },
+    pixel4: {
+      name: "Pixel 4",
+      styles: {
+        width: "353px",
+        height: "745px",
+      },
+    },
+    pixel5: {
+      name: "Pixel 5",
+      styles: {
+        width: "393px",
+        height: "851px",
+      },
+    },
+    samsungGalaxyS8Plus: {
+      name: "Samsung Galaxy S8+",
+      styles: {
+        width: "360px",
+        height: "740px",
+      },
+    },
+    samsungGalaxyS20Ultra: {
+      name: "Samsung Galaxy S20 Ultra",
+      styles: {
+        width: "412px",
+        height: "915px",
+      },
+    },
+    ipadMini: {
+      name: "iPad Mini",
+      styles: {
+        width: "768px",
+        height: "1024px",
+      },
+    },
+    ipadAir: {
+      name: "iPad Air",
+      styles: {
+        width: "820px",
+        height: "1180px",
+      },
+    },
+  };
